1b0a0662306f22353617c6ee9942603c9a86011c
koji_nishimura1 <koji_nishimura@mates-system.com>
2021-09-25 22:59:55 +0900

    Add: #2 parse key and value to string taple

--- src/parse_schema_str.rs
+++ src/parse_schema_str.rs
@@ -1,3 +1,19 @@
+use combine::EasyParser;
+use combine::{
+    // between,
+    error::ParseError,
+    many,
+    many1,
+    parser::char::char,
+    parser::char::{letter, newline, space},
+    // satisfy,
+    // sep_by,
+    Parser,
+    Stream,
+};
+
+use crate::schema::{Data, DataType, FirestoreDataType, Key, Value};
+
 /**
  * main parser
  */
@@ -20,8 +36,41 @@ fn parse_schema_document(document_str: &str) {}
 /**
  * <text> id: Int
  * ↓
- * <hash map> {'id': 'Int'}
+ * <struct> {
+ *    key: { name: "id", optional: false },
+ *    value: Int
+ * }
  */
-fn parse_schema_value_type(value_type_str: &str) {}
+pub fn parse_key_value<Input>() -> impl Parser<Input, Output = (String, String)>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    (
+        many1::<String, _, _>(letter()),
+        many::<String, _, _>(space().or(newline())),
+        char(':'),
+        many::<String, _, _>(space().or(newline())),
+        many1::<String, _, _>(letter()),
+    )
+        .map(|v| (v.0, v.4))
+}
 
-// TODO: unit test
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_key_value() {
+        assert_eq!(
+            Ok((("id".to_string(), "Int".to_string()), "")),
+            parse_key_value().easy_parse("id: Int")
+        )
+    }
+    // let expected = Data {
+    //     key: Key {
+    //         name: String::from("id"),
+    //         optional: false,
+    //     },
+    //     value: Value::Data(DataType::FirestoreDataType(FirestoreDataType::Int)),
+    // };
+}
